3154cd69846021c7a1dde3915cc11ef2ed8ce9d4
PiotrZmuda <piotrzmuda07@gmail.com>
2023-10-03 10:27:49 +0200

    homework done

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -36,20 +36,32 @@
 // export default App
 
 // ### 1. Personal Greeting Card
+// - Create a simple "Greeting Card" component that takes a name prop and displays a message like "Hello, [name]!"
 
 // import React from "react";
 
 // function GreetingCard({ name }) {
 //   return (
 //     <div>
-//       <p>Hello, {"Piotr"}!</p>
+//       <h1>Hello, {name}!</h1>
 //     </div>
 //   );
 // }
 
-// export default GreetingCard;
+// function App() {
+//   return (
+//     <div>
+//       <GreetingCard name="Piotr" />
+//       <GreetingCard name="Ewa" />
+//     </div>
+//   );
+// }
+// export default App;
 
 // ### 2. User Profile
+
+// - Create two components: `ProfilePicture` and `ProfileDetails`. `ProfilePicture` should accept a src prop for the image URL and ProfileDetails
+// should accept name and email props. Then, compose these components inside a parent `UserProfile` component.
 
 // import React from 'react';
 
@@ -92,17 +104,20 @@
 // export default App;
 
 // ### 3. Like Button
+// - Create a `LikeButton` component. It should have a button that displays a count.
+// Every time the button is clicked, the count should increase by one. Use the useState hook for this.
 
 // import { useState } from "react";
 
 // function LikeButton() {
 //   const [count, setCount] = useState(0);
 
+//   const handleButtonClick = () => {
+//     setCount((count) => count + 1);
+//   };
 //   return (
 //     <div>
-//       <button onClick={() => setCount((count) => count + 1)}>
-//         total clicks {count}
-//       </button>
+//       <button onClick={handleButtonClick}>total clicks {count}</button>
 //     </div>
 //   );
 // }
@@ -110,6 +125,8 @@
 // export default LikeButton;
 
 // ### 4. Todo List Item
+// - Build a `Todo` component that displays a task. It should accept the task name as a prop and have a checkbox next to it.
+// When the checkbox is checked, the task name should have a strikethrough.
 
 // import React, { useState } from 'react';
 
@@ -128,13 +145,11 @@
 //   return (
 //     <div>
 //       <label>
-//         {/* Checkbox do zaznaczania/zdjęcia zadania */}
 //         <input
 //           type="checkbox"
 //           checked={isCompleted}
 //           onChange={handleCheckboxChange}
 //         />
-//         {/* Pole tekstowe do wprowadzania zadania */}
 //         <input
 //           type="text"
 //           placeholder="Wpisz zadanie"
@@ -159,7 +174,7 @@
 // function Product({ name, price, description }) {
 //   return (
 //     <div>
-//       <h2>{name}</h2>
+//       <h2>Name: {name}</h2>
 //       <p>Price: {price}</p>
 //       <p>Description: {description}</p>
 //     </div>
@@ -169,19 +184,19 @@
 // function ProductList() {
 //   const products = [
 //     {
-//       name: "Product 1",
-//       price: 19.99,
-//       description: "This is the first product.",
+//       name: "Apples",
+//       price: 2.99,
+//       description: "Apples from Poland",
 //     },
 //     {
-//       name: "Product 2",
-//       price: 29.99,
-//       description: "This is the second product.",
+//       name: "Bananas",
+//       price: 5.99,
+//       description: "bananas from Indonesia.",
 //     },
 //     {
-//       name: "Product 3",
+//       name: "Vodka",
 //       price: 39.99,
-//       description: "This is the third product.",
+//       description: "vodka from Poland.",
 //     },
 //   ];
 
@@ -220,20 +235,20 @@
 //   return (
 //     <div>
 //       <div onClick={toggleAccordion} style={{ cursor: 'pointer' }}>
-//         <h3>Title{title}</h3>
+//         <h3>{title}</h3>
 //       </div>
-//       {!isCollapsed && <div>"content content content content content content content content" {content}</div>}
+//       {!isCollapsed && <div>{content}</div>}
 //     </div>
 //   );
 // }
 
 // function Accordion({ items }) {
 //   return (
-//     <div>
+//     <div >
 //       {items.map((item, index) => (
 //         <AccordionItem
 //           key={index}
-//           title={item.title}
+//           title ={item.title}
 //           content={item.content}
 //         />
 //       ))}
@@ -258,51 +273,26 @@
 
 // function App() {
 //   return (
-//     <div>
+//     <div >
 //       <h1>Accordion Example</h1>
 //       <Accordion items={items} />
 //     </div>
 //   );
 // }
 
-// export default App;
-
-// import React from "react";
-// import InnyKomponent from "./test.jsx";
-// import Button from "./Button.jsx";
-// import "./App.css";
-
-// function Hello() {
-//   return <h1>Witaj piękny Świecie!</h1>;
-// }
-
-// function App() {
-//   return (
-//     <>
-
-//     <h1>{name}</h1>
-//       <div className="myClass">
-//         <p>Devstock rules!!!</p>
-//         <Button text="Wyślij" />
-//       </div>
-//       <div>
-//         <Hello />
-//         <InnyKomponent />
-//         <Button text="Anuluj" />
-//       </div>
-//     </>
-//   );
-// }
-
-// export default App;
+// export default App
 
 // ### 7. Comment System
+
+// - Build a `Comment` component that takes username and commentText as props. Next, make a `CommentList` component that receives an array of comments.
+// This should display a list of comments, demonstrating props drilling as you pass individual comment details down to the `Comment` component.
+// Add a counter at the top of the `CommentList` showing the number of comments.
 
 // import React from "react";
 
 // function Comment({ name, text }) {
 //   return (
-//     <div className="comment">
+//     <div>
 //       <strong>{name}</strong>
 //       {text}
 //     </div>
@@ -402,4 +392,129 @@
 // Components inside `ThemeProvider` should change their background and text colors based on the theme.
 // Use useState to toggle between light and dark themes. Also, implement a `ThemeButton` inside the `ThemeProvider` to toggle the theme.
 
+// import React, { useState } from "react";
 
+// function ThemeProvider() {
+//   const [isDarkMode, setIsDarkMode] = useState(false);
+//   const [buttonText, setButtonText] = useState("Dark mode")
+
+//   const toggleTheme = () => {
+//     setIsDarkMode((prev) => !prev);
+//     setButtonText(prevText => prevText === "Dark mode" ? "White mode" : "Dark mode")
+//   };
+//   const body = document.body;
+//   body.style.backgroundColor = isDarkMode ? "black" : "white";
+
+//   return (
+//     <div>
+//       <button onClick={toggleTheme}>{buttonText}</button>
+//     </div>
+//   );
+// }
+
+// export default ThemeProvider;
+
+// ### 10. Advanced Comment System with Reply
+
+// - Expand the Comment System from #7. For each comment, users should be able to click a "Reply" button to write a response.
+// Replies should be nested under the original comment. The structure would look like this:
+// import React from "react";
+
+import React, { useState } from "react";
+
+const newCommentStyle = {
+  backgroundColor: "lightyellow",
+  padding: "10px",
+  margin: "10px",
+
+};
+
+const postReplayButton = {
+  position: "absolute",
+}
+
+// // Komponent Comment, który wyświetla pojedynczy komentarz
+function Comment({ name, text, index = "", level = 0 }) {
+  const [isReplying, setIsReplying] = useState(false);
+  const [replyText, setReplyText] = useState("");
+  const [replies, setReplies] = useState([]);
+
+  const handleReply = () => {
+    setIsReplying(!isReplying);
+  };
+
+  const handleReplyTextChange = (event) => {
+    setReplyText(event.target.value);
+  };
+
+  const handlePostReply = () => {
+    if (replyText) {
+      setReplies([...replies, { name: "Reply", text: replyText }]);
+      setReplyText("");
+      setIsReplying(false);
+    }
+  };
+
+  return (
+    <div style={level > 0 ? newCommentStyle : null}>
+      <strong>{name} {index}</strong> {text}
+      <button onClick={handleReply}>Reply</button>
+      {isReplying && (
+        <div>
+          <textarea
+            placeholder="Write your reply..."
+            value={replyText}
+            onChange={handleReplyTextChange}
+          />
+          <button style={postReplayButton} onClick={handlePostReply}>Post Reply</button>
+        </div>
+      )}
+      {replies.map((reply, idx) => (
+        <Comment
+          key={idx}
+          name={reply.name}
+          text={reply.text}
+          index={`${index}.${idx + 1}`}
+          level={level + 1}
+        />
+      ))}
+    </div>
+  );
+}
+
+function CommentList({ comments }) {
+  return (
+    <div>
+      <h2>Comments ({comments.length})</h2>
+      {comments.map((comment, index) => (
+        <Comment
+          key={index}
+          name={comment.name}
+          text={comment.text}
+          onReply={(replyText) => {
+            comment.replies = comment.replies || [];
+            comment.replies.push(replyText);
+          }}
+          index={`${index + 1}`}
+        />
+      ))}
+    </div>
+  );
+}
+
+function App() {
+  const comments = [
+    { name: "User1", text: "This is the first comment." },
+    { name: "User2", text: "Another comment here." },
+    { name: "User1", text: "Another comment here." },
+    { name: "User2", text: "Another comment here." },
+  ];
+
+  return (
+    <div>
+      <CommentList comments={comments} />
+    </div>
+  );
+}
+
+export default App;
